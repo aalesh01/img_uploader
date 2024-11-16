d30df43a0fd2546bc29bb2d99149b9bc7b8a3c6e
aalesh01 <aalesh111999@gmail.com>
2024-11-17 00:01:22 +0530

    updated cert

--- utils/storage.js
+++ utils/storage.js
@@ -2,7 +2,14 @@ import { MongoClient } from "mongodb";
 
 const uri =
   "mongodb+srv://aaleshpatilinft:AaAyAdu2EAmWsL7I@cluster0.snq4o.mongodb.net/?retryWrites=true&w=majority&appName=cluster0"; // Replace with your connection string
-const client = new MongoClient(uri);
+
+const client = new MongoClient(uri, {
+  ssl: true,
+  sslValidate: true,
+  sslCA: [fs.readFileSync("../certs/isrgrootx1.pem")],
+  tls: true,
+  tlsAllowInvalidCertificates: false,
+});
 
 let db;
 
